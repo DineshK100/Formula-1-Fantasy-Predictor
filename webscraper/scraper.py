c2f7e1e725abaf51df8c0e6ca06b19805d645df1
drkarnat <drkarnat@ncsu.edu>
2024-06-26 17:31:09 -0400

    Scraper code to get one race data

--- webscraper/scraper.py
+++ webscraper/scraper.py
@@ -1,1 +1,28 @@
-print("Pushing")
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+proceed = True
+current_year = ["2023-bahrain-grand-prix"]
+
+while(proceed):
+    url = "https://pitwall.app/races/2023-bahrain-grand-prix"
+
+    page = requests.get(url)
+
+    soup = BeautifulSoup(page.text, "html.parser")
+
+    table = soup.find('table')
+    headers = [header.text.strip() for header in table.find_all('th')]
+    rows = table.find_all('tr')
+
+    results = []
+
+    for row in rows[1:]:
+        cols = row.find_all('td')
+        cols = [col.text.strip() for col in cols]
+        results.append(cols)
+
+    df = pd.DataFrame(results, columns=headers)
+    print(df)
+   
