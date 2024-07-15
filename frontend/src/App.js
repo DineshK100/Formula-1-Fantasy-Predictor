ba0555e39aadead729ed56e0a231cd6a641f5d10
drkarnat <drkarnat@ncsu.edu>
2024-07-14 21:58:23 -0400

    Learning about css styles. Need to finalize website design

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,11 @@
-import React from 'react';
-import { Route, Routes} from 'react-router-dom'
-import Home from './Home';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Predict from "./Predict";
+import SignUp from "./SignUp";
+import Fantasy from "./Fantasy";
+
+import Header from "./components/Header";
 
 function App() {
   // const [data, setData] = useState({});
@@ -17,10 +22,18 @@ function App() {
 
   // }, []);
 
-  return ( 
-  <Routes>
-    <Route path = "/" element = {<Home/> } />
-  </Routes>
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/predict" element={<Predict />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/fantasy" element={<Fantasy />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
---
+++ frontend/src/Predict.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Predict() {
+    return (
+        <div>
+            <h1>Prediction page</h1>
+            <p>This is the Prediction page</p>
+        </div>
+    );
+}
+
+export default Predict;
