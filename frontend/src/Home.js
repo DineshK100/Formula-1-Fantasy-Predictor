b9015349aa2019fef27ee48dea15142fbe68905a
drkarnat <drkarnat@ncsu.edu>
2024-07-15 13:44:01 -0400

    Designing the Header. Might make it only show like this for the Home page

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Home() {
     return (
-        <div>
+        <div className = "home-content">
             <h1>Home page</h1>
             <p>This is the home page</p>
         </div>
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,33 +1,69 @@
-import React from "react";
-import './Header.css'
+import React, { useEffect, useState } from "react";
+import "./Header.css"; // Assuming you have custom styles
 
 const Header = () => {
+  useEffect(() => {
+    const handleScroll = () => {
+      const navEl = document.querySelector(".navbar");
+      if (window.scrollY >= 56) {
+        navEl.classList.add("navbar-scrolled");
+      } else {
+        navEl.classList.remove("navbar-scrolled");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+  
   return (
-    
-    <header className="header">
-
-      <h1 className="logo">Tempo</h1>
-
-      <div className = "line bottom"></div>
-      <div className = "line diagnol"></div>
-      <div className = "line top"></div>
-
-
-      <nav className="navbar">
-        <ul>
-          <li>
-            <a href="/predict">Predict</a>
-          </li>
-          <li>
-            <a href="/fantasy">Fantasy</a>
-          </li>
-          <li>
-            <a href="/signup">Sign Up</a>
-          </li>
-        </ul>
-      </nav>
-
-    </header>
+    <>
+      <img className="bg-image" src="bg.jpg" alt="Background" />
+        <nav class="navbar navbar-expand-sm navbar-dark fixed-top">
+          <div className="container">
+            <a class="navbar-brand" href="/">
+              GridMaster
+            </a>
+            <button
+              class="navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="/">
+                    Stats
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/fantasy">
+                    Fantasy
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/predict">
+                    Predict
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="/signup">
+                    Sign Up
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+    </>
   );
 };
 
